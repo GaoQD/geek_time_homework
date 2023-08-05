d8be528c5be55173e3557fbd156170448f0aea02
gqd <cap4819@126.com>
2023-08-05 18:07:28 +0800

    Update wework_page.py

--- app_test_auto2/wework/wework_page.py
+++ app_test_auto2/wework/wework_page.py
@@ -1,1 +1,29 @@
+"""
+============================
+@description:TODO
+@author:gaoqd
+@time:2023/7/16  20:58
+@version: 1.0
+@file: wework_page.py
+============================
+"""
+from framework.base_page import BasePage
+
+
+class WeworkPage(BasePage):
+
+    def __init__(self, driver=None, caps=None):
+        caps = {}
+        caps['noReset'] = True
+        caps['appPackage'] = 'com.tencent.wework'
+        caps['appActivity'] = 'com.tencent.wework.launch.LaunchSplashActivity'
+        super().__init__(driver, caps)
+        self.driver.implicitly_wait(5)
+
+    def address_book(self) -> ListPage:
+        self.click(By.XPATH, "//*[@text='通讯录']")
+        return ListPage(self.driver)
+
+    def staging(self):
+        self.click(By.XPATH, "//*[@text='工作台']")
 
