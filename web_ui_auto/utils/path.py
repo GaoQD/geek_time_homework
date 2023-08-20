41d859b3207d815fd8e50fbf5acdd36c68bd41e9
gqd <cap4819@126.com>
2023-08-20 17:00:07 +0800

    Create path.py

---
+++ web_ui_auto/utils/path.py
@@ -0,0 +1,13 @@
+"""
+============================
+@description:TODO
+@author:gaoqd
+@time:2023/8/20  16:34
+@version: 1.0
+@file: path.py
+============================
+"""
+import os
+
+root_path = os.path.dirname(os.path.dirname(__file__))
+data_path = os.path.join(root_path, 'data')
